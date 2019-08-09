6a0cd09bf3e9a39fbc9adebc07179c158ffcdd59
Javier <kachuka99@gmail.com>
2019-08-09 18:51:30 -0300

    Commit Inicial

---
+++ src/persistencia/Persistencia.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
+/**
+ *
+ * @author kachu
+ */
+public class Persistencia {
+        private final EntityManager em;
+
+    public Persistencia(EntityManagerFactory emf) {
+        this.em = emf.createEntityManager();
+    }
+
+    public void iniciarTransaccion() {
+        em.getTransaction().begin();
+    }
+
+    public void confirmarTransaccion() {
+        em.getTransaction().commit();
+    }
+
+    public void descartarTransaccion() {
+        em.getTransaction().rollback();
+    }
+
+    public void insertar(Object o) {
+        this.em.persist(o);
+    }
+
+    public void modificar(Object o) {
+        this.em.merge(o);
+    }
+
+    public void eliminar(Object o) {
+        this.em.remove(o);
+    }
+
+    public void refrescar(Object o) {
+        this.em.refresh(o);
+    }
+
+    public <T extends Object> T buscar(Class<T> clase, Object id) {
+        return (T) this.em.find(clase, id);
+    }
+
+    public <T extends Object> List<T> buscarTodos(Class<T> clase) {
+        CriteriaBuilder cb = this.em.getCriteriaBuilder();
+        CriteriaQuery<T> consulta = cb.createQuery(clase);
+        Root<T> inicio = consulta.from(clase);
+        return em.createQuery(consulta).getResultList();
+    }
+
+    public <T extends Object, P extends Object> List<T> buscarTodosOrdenadosPor(Class<T> clase, SingularAttribute<T, P> orden) {
+        CriteriaBuilder cb = this.em.getCriteriaBuilder();
+        CriteriaQuery<T> consulta = cb.createQuery(clase);
+        Root<T> inicio = consulta.from(clase);
+        consulta.orderBy(cb.asc(inicio.get(orden)));
+        return em.createQuery(consulta).getResultList();
+    }
+
+}
